e72c7dbd8d0fe556ae3de960841678592b4a52e0
maks <nazwiskomaks29@gmail.com>
2026-04-16 14:35:42 +0200

    Pitch/AI Window

---
+++ src/Components/AIWindow.jsx
@@ -0,0 +1,15 @@
+function AIWindow() {
+  return (
+    <>
+      <h1>AI Window</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam illum
+        quibusdam ipsa ullam adipisci veniam soluta, esse qui nobis fugiat
+        laudantium officia exercitationem maxime! Alias nam aliquam autem
+        deserunt perspiciatis.
+      </p>
+    </>
+  );
+}
+
+export default AIWindow;
---
+++ src/Components/PitchWindow.jsx
@@ -0,0 +1,15 @@
+function PitchWindow() {
+  return (
+    <>
+      <h1>Pitch Window</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam illum
+        quibusdam ipsa ullam adipisci veniam soluta, esse qui nobis fugiat
+        laudantium officia exercitationem maxime! Alias nam aliquam autem
+        deserunt perspiciatis.
+      </p>
+    </>
+  );
+}
+
+export default PitchWindow;
