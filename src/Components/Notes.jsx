8249f46e944baa875cc3514e1f1d178890941a6e
maks <nazwiskomaks29@gmail.com>
2026-04-16 14:34:13 +0200

    notes

---
+++ src/Components/Notes.jsx
@@ -0,0 +1,15 @@
+function Notes() {
+  return (
+    <>
+      <h1>Notes</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam illum
+        quibusdam ipsa ullam adipisci veniam soluta, esse qui nobis fugiat
+        laudantium officia exercitationem maxime! Alias nam aliquam autem
+        deserunt perspiciatis.
+      </p>
+    </>
+  );
+}
+
+export default Notes;
