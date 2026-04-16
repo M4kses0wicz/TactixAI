030e8680b2399a105621ff802e0c0d2ad3259607
maks <nazwiskomaks29@gmail.com>
2026-04-16 14:33:30 +0200

    loading screen, main window

---
+++ src/Components/LoadingScreen.jsx
@@ -0,0 +1,11 @@
+function LoadingScreen() {
+  return (
+    <>
+      <h1>TactiX AI</h1>
+      <p>Kliknij w dowolnym miejscu, aby rozpocząć.</p>
+      <img src="#" alt="zdjęcie piłkarza" />
+    </>
+  );
+}
+
+export default LoadingScreen;
---
+++ src/Components/MainWindow.jsx
@@ -0,0 +1,15 @@
+function MainWindow() {
+  return (
+    <>
+      <h1>TactiX AI</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam illum
+        quibusdam ipsa ullam adipisci veniam soluta, esse qui nobis fugiat
+        laudantium officia exercitationem maxime! Alias nam aliquam autem
+        deserunt perspiciatis.
+      </p>
+    </>
+  );
+}
+
+export default MainWindow;
