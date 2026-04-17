747404d4b5ffa189aed6e01584339821552197dc
maks <nazwiskomaks29@gmail.com>
2026-04-17 13:04:56 +0200

    styles

--- src/Components/MainWindow.jsx
+++ src/Components/MainWindow.jsx
@@ -1,13 +1,11 @@
+import PitchWindow from "./PitchWindow";
+
 function MainWindow() {
   return (
     <>
-      <h1>TactiX AI</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam illum
-        quibusdam ipsa ullam adipisci veniam soluta, esse qui nobis fugiat
-        laudantium officia exercitationem maxime! Alias nam aliquam autem
-        deserunt perspiciatis.
-      </p>
+      <main>
+        <PitchWindow />
+      </main>
     </>
   );
 }
