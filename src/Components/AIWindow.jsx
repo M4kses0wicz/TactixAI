321f138ac4911600e2b9334e6d327e2fdc16081a
maks <nazwiskomaks29@gmail.com>
2026-04-21 11:14:39 +0200

    Enhance AIWindow component with new layout and styling; add Material Symbols icon support

--- src/Components/AIWindow.jsx
+++ src/Components/AIWindow.jsx
@@ -4,8 +4,13 @@ import TactixAILogo from "../assets/TactixAI_logo_bez_napisu.png";
 function AIWindow() {
   return (
     <section className="AI-win">
-      <img src={TactixAILogo} alt="Tactix AI Logo" />
-      <div className="d"></div>
+      <img src={TactixAILogo} alt="Tactix AI Logo" className="tactix-logo" />
+      <h2>Witaj!</h2>
+      <p>Jak mogę Ci pomóc?</p>
+      <div className="inp">
+        <input type="text" id="AIInput" placeholder="Zadaj pytanie..." />
+        <span class="material-symbols-outlined">arrow_upward</span>
+      </div>
     </section>
   );
 }
